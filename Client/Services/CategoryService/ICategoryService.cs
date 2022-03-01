a8af00c155f3d4df9afef442dd041d9e4d478cee
Cheryl McIntyre <90618487+cmcintyre-ms@users.noreply.github.com>
2022-03-01 10:46:24 +0000

    Add project files.

---
+++ Client/Services/CategoryService/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using BlazingShop.Shared;
+using System.Collections.Generic;
+
+namespace BlazingShop.Client.Services.CategoryService
+{
+    public interface ICategoryService
+    {
+        List<Category> Categories { get; set; }
+
+        void LoadCategories();
+        
+    }
+}
