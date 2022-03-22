b6a86058a570ca156dfa5012d50b8d10cb5ec791
Cheryl McIntyre <90618487+cmcintyre-ms@users.noreply.github.com>
2022-03-22 09:54:54 +0000

    Completed episode 4

--- Server/Data/DataContext.cs
+++ Server/Data/DataContext.cs
@@ -1,5 +1,7 @@
 ﻿using BlazingShop.Shared;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 
 namespace BlazingShop.Server.Data
 {
@@ -12,6 +14,7 @@ namespace BlazingShop.Server.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Edition> Editions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -30,7 +33,8 @@ namespace BlazingShop.Server.Data
                      Description = "The Hitchhiker's Guide to the Galaxy (sometimes referred to as HG2G, HHGTTG, H2G2, or tHGttG) is a comedy science fiction series created by Douglas Adams.",
                      Image = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
                      Price = 9.99m,
-                     OriginalPrice = 19.99m
+                     OriginalPrice = 19.99m,
+                     DateCreated = new DateTime(2022, 1, 1)
                  },
                 new Product
                 {
@@ -40,7 +44,8 @@ namespace BlazingShop.Server.Data
                     Description = "Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopia in 2045, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which would lead him to inherit the game creator's fortune.",
                     Image = "https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg",
                     Price = 15.99m,
-                    OriginalPrice = 20.99m
+                    OriginalPrice = 20.99m,
+                    DateCreated = new DateTime(2022, 1, 1)
                 },
                 new Product
                 {
@@ -49,7 +54,8 @@ namespace BlazingShop.Server.Data
                     Title = "Nineteen Eighty-Four",
                     Description = "Nineteen Eighty-Four: A Novel, often published as 1984, is a dystopian social science fiction novel by English novelist George Orwell. It was published on 8 June 1949 by Secker & Warburg as Orwell's ninth and final book completed in his lifetime.",
                     Image = "https://upload.wikimedia.org/wikipedia/commons/c/c3/1984first.jpg",
-                    Price = 5.99m
+                    Price = 5.99m,
+                    DateCreated = new DateTime(2022, 1, 1)
                 },
                 new Product
                 {
@@ -59,7 +65,8 @@ namespace BlazingShop.Server.Data
                     Description = "The Pentax Spotmatic refers to a family of 35mm single-lens reflex cameras manufactured by the Asahi Optical Co. Ltd., later known as Pentax Corporation, between 1964 and 1976.",
                     Image = "https://upload.wikimedia.org/wikipedia/commons/e/e9/Honeywell-Pentax-Spotmatic.jpg",
                     Price = 150.50m,
-                    OriginalPrice = 200.00m
+                    OriginalPrice = 200.00m,
+                    DateCreated = new DateTime(2022, 1, 1)
                 },
                 new Product
                 {
@@ -69,7 +76,8 @@ namespace BlazingShop.Server.Data
                     Description = "The Xbox is a home video game console and the first installment in the Xbox series of video game consoles manufactured by Microsoft.",
                     Image = "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox-console.jpg",
                     Price = 125.99m,
-                    OriginalPrice = 199.99m
+                    OriginalPrice = 199.99m,
+                    DateCreated = new DateTime(2022, 1, 1)
                 },
                 new Product
                 {
@@ -78,7 +86,8 @@ namespace BlazingShop.Server.Data
                     Title = "Super Nintendo Entertainment System",
                     Description = "The Super Nintendo Entertainment System (SNES), also known as the Super NES or Super Nintendo, is a 16-bit home video game console developed by Nintendo that was released in 1990 in Japan and South Korea.",
                     Image = "https://upload.wikimedia.org/wikipedia/commons/e/ee/Nintendo-Super-Famicom-Set-FL.jpg",
-                    Price = 125.50m
+                    Price = 125.50m,
+                    DateCreated = new DateTime(2022, 1, 1)
                 },
                 new Product
                 {
@@ -88,7 +97,8 @@ namespace BlazingShop.Server.Data
                     Description = "Half-Life 2 is a 2004 first-person shooter game developed and published by Valve. Like the original Half-Life, it combines shooting, puzzles, and storytelling, and adds features such as vehicles and physics-based gameplay.",
                     Image = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
                     Price = 25.99m,
-                    OriginalPrice = 35.99m
+                    OriginalPrice = 35.99m,
+                    DateCreated = new DateTime(2022, 1, 1)
                 },
                 new Product
                 {
@@ -98,7 +108,8 @@ namespace BlazingShop.Server.Data
                     Description = "Diablo II is an action role-playing hack-and-slash computer video game developed by Blizzard North and published by Blizzard Entertainment in 2000 for Microsoft Windows, Classic Mac OS, and macOS.",
                     Image = "https://upload.wikimedia.org/wikipedia/en/d/d5/Diablo_II_Coverart.png",
                     Price = 15.99m,
-                    OriginalPrice = 20.99m
+                    OriginalPrice = 20.99m,
+                    DateCreated = new DateTime(2022, 1, 1)
                 },
                 new Product
                 {
@@ -107,8 +118,29 @@ namespace BlazingShop.Server.Data
                     Title = "Day of the Tentacle",
                     Description = "Day of the Tentacle, also known as Maniac Mansion II: Day of the Tentacle, is a 1993 graphic adventure game developed and published by LucasArts. It is the sequel to the 1987 game Maniac Mansion.",
                     Image = "https://upload.wikimedia.org/wikipedia/en/7/79/Day_of_the_Tentacle_artwork.jpg",
-                    Price = 5.99m
+                    Price = 5.99m,
+                    DateCreated = new DateTime(2022, 1, 1)
                 }
+            );
+
+            modelBuilder.Entity<Edition>().HasData(
+                    new Edition { Id = 1, Name = "Paperback" },
+                    new Edition { Id = 2, Name = "E-book" },
+                    new Edition { Id = 3, Name = "Audiobook" },
+                    new Edition { Id = 4, Name = "PC" },
+                    new Edition { Id = 5, Name = "PlayStation" },
+                    new Edition { Id = 6, Name = "Xbox" }
+                );
+
+            modelBuilder.SharedTypeEntity<Dictionary<string, object>>("EditionProduct").HasData(
+                    new { EditionsId = 1, ProductsId = 1 },
+                    new { EditionsId = 2, ProductsId = 1 },
+                    new { EditionsId = 3, ProductsId = 1 },
+                    new { EditionsId = 1, ProductsId = 2 },
+                    new { EditionsId = 2, ProductsId = 2 },
+                    new { EditionsId = 4, ProductsId = 7 },
+                    new { EditionsId = 5, ProductsId = 7 },
+                    new { EditionsId = 6, ProductsId = 7 }
                 );
         }
     }
---
+++ Server/Migrations/20220321111129_ProductDateFix.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazingShop.Server.Migrations
+{
+    public partial class ProductDateFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Products",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Products",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateUpdated",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 3, 16, 15, 56, 55, 669, DateTimeKind.Local).AddTicks(7517), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 3, 16, 15, 56, 55, 672, DateTimeKind.Local).AddTicks(9013), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 3, 16, 15, 56, 55, 672, DateTimeKind.Local).AddTicks(9048), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 3, 16, 15, 56, 55, 672, DateTimeKind.Local).AddTicks(9053), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 3, 16, 15, 56, 55, 672, DateTimeKind.Local).AddTicks(9056), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 3, 16, 15, 56, 55, 672, DateTimeKind.Local).AddTicks(9060), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 3, 16, 15, 56, 55, 672, DateTimeKind.Local).AddTicks(9063), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 3, 16, 15, 56, 55, 672, DateTimeKind.Local).AddTicks(9066), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "DateCreated", "DateUpdated" },
+                values: new object[] { new DateTime(2022, 3, 16, 15, 56, 55, 672, DateTimeKind.Local).AddTicks(9069), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+    }
+}
