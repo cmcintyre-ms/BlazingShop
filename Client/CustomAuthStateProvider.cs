4187d9c8ef1fd76fdb0d2d22d9ce399be4a7ce1b
Cheryl McIntyre <90618487+cmcintyre-ms@users.noreply.github.com>
2022-03-23 14:32:26 +0000

    Completed episode 7

---
+++ Client/CustomAuthStateProvider.cs
@@ -0,0 +1,38 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Client
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService localStorage;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage)
+        {
+            this.localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var state = new AuthenticationState(new ClaimsPrincipal());
+
+            string username = await localStorage.GetItemAsStringAsync("username");
+            if (!string.IsNullOrEmpty(username))
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, username)
+                }, "Test authentication type");
+
+                state = new AuthenticationState(new ClaimsPrincipal(identity));
+            }
+
+            NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+            return state;
+        }
+    }
+}
